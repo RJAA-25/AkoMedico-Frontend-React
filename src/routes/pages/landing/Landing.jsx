0b37a8e7d87afac0afaa80adef4fa4a9f55bee94
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-11-26 11:09:35 +0800

    Remove border from Landing page

--- src/routes/pages/landing/Landing.jsx
+++ src/routes/pages/landing/Landing.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const Landing = () => {
 	return (
-		<div className="flex-grow-1 border d-flex flex-column justify-content-center align-items-center mb-5">
+		<div className="flex-grow-1  d-flex flex-column justify-content-center align-items-center mb-5">
 			<Link to="/login" className="btn btn-warning btn-lg fs-6 px-5 m-3 rounded-pill">
 				Login
 			</Link>
